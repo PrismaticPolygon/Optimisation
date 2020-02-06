e83144596efd88d48cfb597abb00ceea2011934b
PrismaticPolygon <domwhite97@gmail.com>
2020-02-06 14:00:29 +0000

    Added report

---
+++ q4c.py
@@ -0,0 +1,29 @@
+from pulp import *
+
+problem = LpProblem("q4", LpMaximize)
+
+A = LpVariable("A", 0, 1, LpInteger)
+B = LpVariable("B", 0, 1, LpInteger)
+C = LpVariable("C", 0, 1, LpInteger)
+D = LpVariable("D", 0, 1, LpInteger)
+E = LpVariable("E", 0, 1, LpInteger)
+F = LpVariable("F", 0, 1, LpInteger)
+w = LpVariable("Excess", 0, None, LpInteger)
+
+weight = 6 * A + 7 * B + 4 * C + 9 * D + 3 * E + 8 * F + w
+
+problem += (60 * A + 70 * B + 40 * C + 70 * D + 16 * E + 100 * F) - 15 * w, "Value of items"
+
+problem += D - C >= 0, "Constraint"
+
+# Nope. I need a break.
+
+problem.solve()
+
+print("Status:", LpStatus[problem.status])
+
+for v in problem.variables():
+
+    print(v.name, "=", v.varValue)
+
+print("Value of items:", value(problem.objective))
